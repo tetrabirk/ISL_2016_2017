0e5a7bb380e21d310f1a2f720ab000f5b7540f3d
leyensl <leyensl@ccmp05.isl.be>
2016-10-04 18:35:02 +0200

    on repart a zero

---
+++ creasite/index.php
@@ -0,0 +1,18 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>creasite</title>
+    </head>
+    <body>
+        <ul>
+            <li><a href="2016_09_12_ Rappels_form/exe3.html"></a></li>
+            <li><a href="2016_09_12_ Rappels_form/exe3_correction.html"></a></li>
+            <li><a href="2016_09_13_Rappels_form+selecteur_avance/exe3.html"></a></li>
+            <li><a href="2016_09_13_Rappels_form+selecteur_avance/exe5_form1.html"></a></li>
+            <li><a href="2016_09_13_Rappels_form+selecteur_avance/exe6_form2.html"></a></li>
+        </ul>
+    </body>
+</html>
