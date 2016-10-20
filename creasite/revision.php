bdc8d79a324e87a4f3a4b0ad55719d0a8e32a27e
Tetrabirk <ludovic.leyens@gmail.Com>
2016-10-20 15:32:56 +0200

    revision

---
+++ creasite/revision.php
@@ -0,0 +1,87 @@
+<?php
+?>
+
+<html>
+    <head>
+        <title>revision</title>
+    </head>
+    <body>
+        <form name="formulaire" method="get" action="#">
+            <label for="text">input type text</label>
+            <input type="text" name="text" id="text">
+            <br/>
+            <label for="number">input type number</label>
+            <input type="number" name="number" id="number">
+            <br/>
+            <label for="email">input type email</label>
+            <input type="email" name="email" id="email">
+            <br/>
+            <label for="url">input type url</label>
+            <input type="url" name="url" id="url">
+            <br/>
+            <label for="tel">input type tel</label>
+            <input type="tel" name="tel" id="tel">
+            <br/>
+            <label for="date">input type date</label>
+            <input type="date" name="date" id="date">
+            <br/>
+            <label for="color">input type color</label>
+            <input type="color" name="color" id="color">
+            <br/>
+            <label for="range">input type range</label>
+            <input type="range" name="range" id="range">
+            <br/>
+            <label for="hidden">input type hidden</label>
+            <input type="hidden" name="hidden" id="hidden">
+            <br/>
+            <label for="file"> input type file</label>
+            <input type="file" name="file" id="file">
+            <br/>
+            <label for="password">input type password</label>
+            <input type="password" name="password" id="password">
+            <br/>
+            <label for="search">input type search</label>
+            <input type="search" name="search" id="search">
+            <br/>
+            <label for="checkbox">input type checkbox</label>
+            <input type="checkbox" name="checkbox" id="checkbox" value="1">UN
+            <input type="checkbox" name="checkbox" id="checkbox" value="2">DEUX
+            <br/>
+            <label for="radio">input type radio</label>
+            <input type="radio" name="radio" id="radio" value="1">UN
+            <input type="radio" name="radio" id="radio" value="2">DEUX
+            <br/>
+            <label for="textarea">textarea</label>
+            <textarea id="textarea" name="textarea"></textarea>
+            <br/>
+            <label for="select">select</label>
+            <select name="select" id="select">
+                <option value="1">UN</option>
+                <option value="2">DEUX</option>
+            </select>
+            <br/>
+            <label for="datalist">input type text + datalist</label>
+            <input list="data" input="text" name="datalist" id="datalist">
+            <datalist id="data">
+                <option value="1">
+                <option value="2">
+                <option value="3">
+            </datalist>
+            <br/>
+            
+            
+            
+            
+            <label for="submit"> input type submit</label>
+            <input type="submit" id="submit" name="submit" value="submit">
+            <br/>
+            <label for="reset"> input type reset</label>
+            <input type="reset" id="reset" name="reset" value="reset">
+            
+            
+            
+            
+            
+        </form>
+    </body>
+</html>
