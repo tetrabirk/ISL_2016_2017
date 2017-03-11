15f36818a73929739d8533fc129bfa53fd170e35
tetrabirk <ludovic.leyens@gmail.com>
2017-03-11 14:40:50 +0100

    moitier d'un devoir

--- POO_FrameWork/2017_03_07_exe_composer_namespace_autoloading/index.php
+++ POO_FrameWork/2017_03_07_exe_composer_namespace_autoloading/index.php
@@ -1,4 +1,13 @@
 <?php
+require_once __DIR__.'./src/Entity/Personne.php';
+require_once __DIR__.'./src/Manager/PersonneManager.php';
+require_once __DIR__.'./vendor/autoload.php';
+
+$test = new \ISL\Manager\PersonneManager();
+
+print_r($test->create(2));
+
+
 
 ?>
 <!DOCTYPE html>
--- POO_FrameWork/2017_03_07_exe_composer_namespace_autoloading/src/Manager/PersonneManager.php
+++ POO_FrameWork/2017_03_07_exe_composer_namespace_autoloading/src/Manager/PersonneManager.php
@@ -4,6 +4,42 @@ namespace ISL\Manager;
 
 class PersonneManager{
     
+    public function randomPerson(){
+        $person = [];
+        $faker= \Faker\Factory::create('fr_BE');
+        
+        $person['nom']    =$faker->lastName;
+        $person['prenom'] =$faker->firstName;
+        $person['adresse'] =$faker->streetAddress;
+        $person['cp'] =$faker->postCode;
+        $person['pays'] =$faker->country;
+        $person['societe'] =$faker->company;
+        
+        return $person;
+        
+    }
+    public function create($nbre){
+        $groupe = [];
+        for ($i=0; $i<$nbre; $i++){
+            $person = $this->randomPerson();
+            
+            $groupe[$i]= new \Personne();
+            
+            $groupe[$i]->setNom($person['nom']);
+            $groupe[$i]->setPrenom($person['prenom']);
+            $groupe[$i]->setAdresse($person['adresse']);
+            $groupe[$i]->setCp($person['cp']);
+            $groupe[$i]->setPays($person['pays']);
+            $groupe[$i]->setSociete($person['societe']);
+            
+        }
+        return $groupe;
+        
+    }
+    
+    
+    
 }
 
+    
 ?>
