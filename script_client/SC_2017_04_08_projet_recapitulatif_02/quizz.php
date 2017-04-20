f827f2c89e4e39523ab8f8e8309204115a3918e4
Tetrabirk <ludovic.leyens@gmail.Com>
2017-04-20 17:13:43 +0200

    dsezgqzgqz

---
+++ script_client/SC_2017_04_08_projet_recapitulatif_02/quizz.php
@@ -0,0 +1,58 @@
+<?php
+
+// Questions And Answers
+$questions = array(
+    '1' => array(
+        'question' => 'En quelle année eut lieu la bataille de Waterloo?',
+        'propositions' => array(
+            '1805',
+            '1815',
+            '1830'
+        ),
+        'correct' => 2
+    ),
+    '2' => array(
+        'question' => 'Combien d\'années a duré la guerre de cent ans ?',
+        'propositions' => array(
+            '92',
+            '100',
+            '116'
+        ),
+        'correct' => 3
+    ),
+    '3' => array(
+        'question' => 'Combien de titres de champion de Belgique de football Anderlecht a-t-il remportés ?',
+        'propositions' => array(
+            '29',
+            '33',
+            '35'
+        ),
+        'correct' => 2
+    ),
+    '4' => array(
+        'question' => 'Quelle est la longueur du circuit de Francorchamps ?',
+        'propositions' => array(
+            '7004 m',
+            '7142 m ',
+            '7215 m'
+        ),
+        'correct' => 1
+    ),
+    '5' => array(
+        'question' => 'Combien de titres Nadal a-t-il remporté en senior à Roland-Garros ?',
+        'propositions' => array(
+            '7',
+            '8',
+            '9'
+        ),
+        'correct' => 3
+    )
+);
+$return = array();
+if (isset($_GET['question'])) {
+    if (array_key_exists($_GET['question'], $questions)) {
+        $return = $questions[$_GET['question']];
+    }
+}
+
+echo json_encode($return);
---
+++ script_client/SC_2017_04_08_projet_recapitulatif_02/reflexion.js
@@ -0,0 +1,49 @@
+/* passage question ou correction?
+ *   question?
+ *          no question?
+ *          afficher question
+ *          afficher réponse
+ *          fond bleu
+ *          
+ *          premier question?
+ *              ne pas afficher "précédent"
+ *          sinon
+ *              afficher "précédent"
+ *          
+ *          tt les réponses on été donnée?
+ *              afficher "valider"
+ *          sinon
+ *              ne pas afficher "valider"
+ *                  
+ *          //lorsqu'on clic sur le bouton "suivant"
+ *              est ce qu'une réponse à été cochée?
+ *                  changer no question
+ *              sinon
+ *                  fond rouge
+ *                  afficher msg "vueillez répondre a la question"
+ *         //lorsqu'on clic sur le bouton " valider"
+ *   correction?
+ *          no question?
+ *          afficher question
+ *          afficher réponse + fond vert ou rouge pour$$$$$$$$$$$$$$$$$$$$$$$$$$
+ *          fond bleu
+ *          
+ *          premier question?
+ *              ne pas afficher "précédent"
+ *          sinon
+ *              afficher "précédent"
+ *          
+ *          tt les réponses on été donnée?
+ *              afficher "valider"
+ *          sinon
+ *              ne pas afficher "valider"
+ *                  
+ *          //lorsqu'on clic sur le bouton "suivant"
+ *              est ce qu'une réponse à été cochée?
+ *                  changer no question
+ *              sinon
+ *                  fond rouge
+ *                  afficher msg "vueillez répondre a la question"
+ *         //lorsqu'on clic sur le bouton " valider"
+ *          
+ */
