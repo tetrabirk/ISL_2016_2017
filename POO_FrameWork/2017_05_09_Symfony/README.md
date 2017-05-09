e41b1d3127a1a3274e4ee3094918381e511d2c37
leyensl <leyensl@ccmp05.isl.be>
2017-05-09 21:21:58 +0200

    fin du cours

---
+++ POO_FrameWork/2017_05_09_Symfony/README.md
@@ -0,0 +1,4 @@
+2017_05_09_Symfony
+==================
+
+A Symfony project created on May 9, 2017, 7:30 pm.
---
+++ POO_FrameWork/Agenda.php
@@ -0,0 +1,9 @@
+05/09 - intro et création d'un projet symfony
+05/16 - presentation concepts: Appkernel, Routing Controller Response Request
+05/23 - Présentation du moteur de template: Twig
+05-30 - presentation de Doctrine: entités + relations (repo+DQL)
+06/01 - mise en appli des concepts vus
+06-06 - présentation du composant Form: les formulaires et la validation
+06/08 - application des concetps
+06/13 - application des concepts +Q/R
+06/15 - Evaluation
