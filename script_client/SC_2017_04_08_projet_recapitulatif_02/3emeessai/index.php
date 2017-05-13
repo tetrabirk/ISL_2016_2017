557f7a3534a7d25a28afe24cd54fa0788fcd4411
tetrabirk <ludovic.leyens@gmail.com>
2017-05-13 22:55:18 +0200

    revision

---
+++ script_client/SC_2017_04_08_projet_recapitulatif_02/3emeessai/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Quizz 2015</title>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+    <body>
+        
+        <button id="start" value='start'>commencer le quizz</button>
+        <form id='quizz'>
+            <input id='rep1' type='radio' value='1' name='reponses'>            
+            <input id='rep2' type='radio' value='2' name='reponses'>            
+            <input id='rep3' type='radio' value='3' name='reponses'>            
+        </form>
+        
+        
+        <script src="js/jquery-3.1.1.js" type="text/javascript"></script>
+        <script src="js/main.js" type="text/javascript"></script>
+    </body>
+</html>
